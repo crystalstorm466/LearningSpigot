d912d8c58bed083b3bc5cc4a2cfa3384cc9e295f
David Holland <DcHolland_89@msn.com>
2022-12-20 23:22:52 -0500

    added the ability to give kits to other players

--- src/me/david/LearningSpigot/CommandKit.java
+++ src/me/david/LearningSpigot/CommandKit.java
@@ -1,8 +1,8 @@
 package me.david.LearningSpigot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,39 +21,53 @@ public class CommandKit implements CommandExecutor {
 
         if (sender instanceof Player) {
             Player player = (Player) sender;
+            if (args.length == 1) {
+                Player target = Bukkit.getPlayer(args[0]);
+                if (!(target == null)) {
+                    giveItems(target);
+                } else {
+                    player.sendMessage(ChatColor.RED + "Unable to find player!");
+                }
+            } else {
+                giveItems(player);
+            }
+            return true;
             //give items
-            ItemStack boat = new ItemStack(Material.ACACIA_BOAT);
-            ItemStack bricks = new ItemStack(Material.BRICK);
-            ItemStack netherite = new ItemStack(Material.NETHERITE_BLOCK, 10); //alt way
-            bricks.setAmount(200);
-
-            player.getInventory().addItem(bricks, boat, netherite);
-            ItemStack award = new ItemStack(Material.TOTEM_OF_UNDYING);
-            award.addUnsafeEnchantment(Enchantment.DURABILITY, 1);
-            ItemMeta awardData = award.getItemMeta();
-            awardData.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-            String loreList[] = new String[3];
-            loreList[0] = (ChatColor.GOLD + "Founder's Item: Learning Spigot");
-            loreList[1] = (ChatColor.GOLD + "Awarded to: Evangeline_Samos who developed this plugin.");
-            loreList[2] = (ChatColor.GOLD + "This award is highly prestigious\n only a select few has the honor of receiving it.");
-            awardData.setLore(Arrays.asList(loreList));
-            awardData.setDisplayName( ChatColor.RED + "Plugin Development - Learning Spigot - " + player.getName());
-            award.setItemMeta(awardData);
-
-            player.getInventory().addItem(award);
-
-            ItemStack weapon = new ItemStack(Material.STICK);
-            weapon.addUnsafeEnchantment(Enchantment.KNOCKBACK, 10);
-            ItemMeta weaponData = weapon.getItemMeta();
-            String loreListWeapon[] = new String[3];
-            loreListWeapon[0] = (ChatColor.DARK_PURPLE + "Weapon of Back de Knock");
-            loreListWeapon[1] = (ChatColor.DARK_GREEN + "Can knockback players a great distance.");
-            weaponData.setLore(Arrays.asList(loreListWeapon));
-            weaponData.setDisplayName(ChatColor.GOLD + "Plugin Development - Learning Spigot - Knockback Weapon");
-            weapon.setItemMeta(weaponData);
-            player.getInventory().addItem(weapon);
         }
         return true;
+    }
+
+    private void giveItems(Player target) {
+        ItemStack boat = new ItemStack(Material.ACACIA_BOAT);
+        ItemStack bricks = new ItemStack(Material.BRICK);
+        ItemStack netherite = new ItemStack(Material.NETHERITE_BLOCK, 10); //alt way
+        bricks.setAmount(200);
+
+        target.getInventory().addItem(bricks, boat, netherite);
+        ItemStack award = new ItemStack(Material.TOTEM_OF_UNDYING);
+        award.addUnsafeEnchantment(Enchantment.DURABILITY, 1);
+        ItemMeta awardData = award.getItemMeta();
+        awardData.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        String[] loreList = new String[3];
+        loreList[0] = (ChatColor.GOLD + "Founder's Item: Learning Spigot");
+        loreList[1] = (ChatColor.GOLD + "Awarded to: Evangeline_Samos who developed this plugin.");
+        loreList[2] = (ChatColor.GOLD + "This award is highly prestigious\n only a select few has the honor of receiving it.");
+        awardData.setLore(Arrays.asList(loreList));
+        awardData.setDisplayName( ChatColor.RED + "Plugin Development - Learning Spigot - " + target.getName());
+        award.setItemMeta(awardData);
+
+        target.getInventory().addItem(award);
+
+        ItemStack weapon = new ItemStack(Material.STICK);
+        weapon.addUnsafeEnchantment(Enchantment.KNOCKBACK, 10);
+        ItemMeta weaponData = weapon.getItemMeta();
+        String[] loreListWeapon = new String[3];
+        loreListWeapon[0] = (ChatColor.DARK_PURPLE + "Weapon of Back de Knock");
+        loreListWeapon[1] = (ChatColor.DARK_GREEN + "Can knockback players a great distance.");
+        weaponData.setLore(Arrays.asList(loreListWeapon));
+        weaponData.setDisplayName(ChatColor.GOLD + "Plugin Development - Learning Spigot - Knockback Weapon");
+        weapon.setItemMeta(weaponData);
+        target.getInventory().addItem(weapon);
     }
 
 

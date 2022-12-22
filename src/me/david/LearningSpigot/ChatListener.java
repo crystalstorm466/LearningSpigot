6107453671cf7ccc3b299bd8f441ac97bf9a89d7
David Holland <DcHolland_89@msn.com>
2022-12-21 22:23:53 -0500

    fixed the swear filter however it suffers from clbuttic where classic or words with swears inside of them get flagged. But it works in theory now.

--- src/me/david/LearningSpigot/ChatListener.java
+++ src/me/david/LearningSpigot/ChatListener.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
@@ -12,37 +13,38 @@ public class ChatListener implements Listener {
 
     public ChatListener(LearningSpigot plugin) {
         this.Plugin = plugin;
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onPlayerChat(AsyncPlayerChatEvent event) {
 
-        final LearningSpigot plugin = new LearningSpigot();
-        Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
-
         String swearWords[] = new String[13];
-        swearWords[1] = "heck";
-        swearWords[2] = "hell";
-        swearWords[3] = "ass";
-        swearWords[4] = "bitch";
-        swearWords[5] = "asshole";
-        swearWords[6] = "fuck";
-        swearWords[7] = "frick";
-        swearWords[8] = "feck";
-        swearWords[9] = "asshole";
-        swearWords[10] = "pussy";
-        swearWords[11] = "porn";
+        swearWords[0] = "heck";
+        swearWords[1] = "hell";
+        swearWords[2] = "ass";
+        swearWords[3] = "bitch";
+        swearWords[4] = "asshole";
+        swearWords[5] = "fuck";
+        swearWords[6] = "frick";
+        swearWords[7] = "feck";
+        swearWords[8] = "asshole";
+        swearWords[9] = "pussy";
+        swearWords[10] = "porn";
 
         Player player = event.getPlayer();
         String message = event.getMessage();
         for (int i = 0; i < swearWords.length; i++) {
-            if (message.equalsIgnoreCase(swearWords[i])) {
+
+            message = message.toLowerCase();
+            if (message.contains(swearWords[i])) {
                 event.setCancelled(true);
                 player.sendMessage(ChatColor.RED + "Your message was blocked because you swore.");
 
+                /*
                 Bukkit.getServer().broadcastMessage(ChatColor.RED + player.getName() + " tried to say " +
-                        "a swear word in chat and has ruined the chat for everyone.");
+                        "a swear word in chat and has ruined the chat for everyone."
+                 */
+
                 break;
             }
         }
